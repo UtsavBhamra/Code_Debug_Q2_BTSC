025f24d1e05bfdd66d02232adb8abcd131d967e8
UtsavBhamra <utsavbhamra2005@gmail.com>
2024-07-14 20:54:19 +0530

    Initial commit

--- TypingTutor/app/controllers/typing_test_controller.rb
+++ TypingTutor/app/controllers/typing_test_controller.rb
@@ -1,11 +1,11 @@
 class TypingTestController < ApplicationController
   def index
-    @paragraph = random_paragraph
+    @paragraph = random_paragraph.find(params[:id]).content
   end
 
   private
 
   def random_paragraph
-    paragraphs = Paragraph.all
+    paragraphs = Paragraph
   end
 end
--- TypingTutor/config/routes.rb
+++ TypingTutor/config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root 'typing_test#index'
+  get '/:id', to: 'typing_test#index'
 end
